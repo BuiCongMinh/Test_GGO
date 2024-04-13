c89e6866c5cda8d02c3fa92bc72825f18ca0a7be
Test_Terminal_git <minhvnvnvn1998@gmail.com>
2024-04-13 14:01:26 +0700

    hoan thanh sector_4

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
-const scrollToFooterBtn = document.querySelector("#scrollToFooterBtn");
-const contact = document.querySelector(".contact");
-const sector1 = document.querySelector(".sector_1");
-const btnDown = document.querySelector("#btnDown");
-
-scrollToFooterBtn.addEventListener("click", () => {
-  contact.scrollIntoView({ behavior: "smooth" });
-});
-
-btnDown.addEventListener("click", () => {
-  sector1.scrollIntoView({ behavior: "smooth" });
-});
-
-
-const scrollSmoothlyToBottom = (id) => {
-    // console.log(12);
-  const element = $(`#${id}`);
-  console.log(12,element);
-  element.animate(
-    {
-      scrollTop: element.prop("scrollHeight"),
-    },
-    3000
-  );
+const scrollSmoothlyToBottom = (to) => {
+  $("html, body").animate({ scrollTop: $(`#${to}`).offset().top }, 1000);
 };
 
-
 // sorlling sector_2
-let coppy = document.querySelector('.logos_slide').cloneNode(true);
-document.querySelector('.scroll_partner').appendChild(coppy);
+let coppy = document.querySelector(".logos_slide").cloneNode(true);
+document.querySelector(".scroll_partner").appendChild(coppy);
 
+//sorlling sector_4
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+let owl = $(".owl-carousel");
+console.log(owl);
+owl.owlCarousel({
+  items: 4,
+  center: true,
+  loop: true,
+  margin: 12,
+  autoplay: true,
+  autoplayTimeout: 3500,
+  autoplaySpeed: 1000,
+  autoplayHoverPause: true,
+  dots: true,
+  dotsSpeed: 1000,
+  responsive: {
+    0: {
+      items: 2,
+      center: false,
+    },
+    600: {
+      items: 4,
+    },
+  },
+});
 
-
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [1000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
